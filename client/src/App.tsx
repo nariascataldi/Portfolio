05c5de8e8ad06c7a6def5d92c633beb65e767af7
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-10-31 10:47:31 -0300

    tasks app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,27 @@
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import LandingPage from "./components/LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from './app/store';
+
 import ReactInit from './components/reactInit/ReactInit';
+import LandingPage from "./components/LandingPage";
+import Home from "./components/NavScroll";
+import Task from "./components/Task";
+import TasksForm from "./components/TasksForm";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <Provider store={store}>
         <Routes>
-          <Route path='/' element={<LandingPage />} />
           <Route path='/reactInit' element={<ReactInit />} />
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/task' element={<Task />} />
+          <Route path='/create_task' element={<TasksForm />} />
+          <Route path='/edit_task/:id' element={<TasksForm />} />
         </Routes>
-      </div>
+      </Provider>
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Task.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import TasksList from "./TasksList";
+function Task() {
+
+  return (
+    <Fragment>
+      {/* <h1>Task</h1> */}
+      <TasksList />
+    </Fragment>
+
+  )
+}
+export default Task;
