f75484aed9f5c9c45408cee41f10ce8f147e1521
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-11-23 19:36:00 -0300

    01 family

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -5,8 +5,9 @@ export const Home = () => {
   return (
     <>
       <div className="container">
-        {/* // -- Seccion parallax -- */}
-        <section className="parallax-bg img_perro1" > </section>
+
+        {/* family */}
+        <section className="parallax-bgF family_tv"></section>
         <section className="parallax-content">
           <div>
             <h1>Parallax section</h1>
@@ -21,21 +22,12 @@ export const Home = () => {
             <p>En esta sección incluimos la clase "bg-1" para otorgarle un color de fondo.</p>
           </div>
         </section>
-
-        {/* // -- Seccion parallax */}
-        <section className="parallax-bg img_perro2"> </section>
+        {/* // -- Seccion parallax -- */}
+        <section className="parallax-bg img_perro1" > </section>
         <section className="parallax-content">
           <div>
             <h1>Parallax section</h1>
             <p>Este texto acompaña al background parallax en su propia sección</p>
-          </div>
-        </section>
-
-        {/* // -- Seccion NO parallax */}
-        <section className="no-parallax-section bg-2">
-          <div className="no-parallax-content">
-            <h1>No-parallax section</h1>
-            <p>En esta sección incluimos la clase "bg-2" para otorgarle un color de fondo.</p>
           </div>
         </section>
 
@@ -45,3 +37,20 @@ export const Home = () => {
   )
 }
 
+
+        // {/* // -- Seccion parallax */}
+        // <section className="parallax-bg img_perro2"> </section>
+        // <section className="parallax-content">
+        //   <div>
+        //     <h1>Parallax section</h1>
+        //     <p>Este texto acompaña al background parallax en su propia sección</p>
+        //   </div>
+        // </section>
+
+        // {/* // -- Seccion NO parallax */}
+        // <section className="no-parallax-section bg-2">
+        //   <div className="no-parallax-content">
+        //     <h1>No-parallax section</h1>
+        //     <p>En esta sección incluimos la clase "bg-2" para otorgarle un color de fondo.</p>
+        //   </div>
+        // </section>
