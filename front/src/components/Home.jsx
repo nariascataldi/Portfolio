b0a00e55d713a2925da7e38b042961994f4a1a18
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-11-24 18:01:51 -0300

    home

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,36 +1,30 @@
 import '../styles/Home.css';
-
+import family_tv from '../assets/image/parallax/family-watch-tv.png';
 export const Home = () => {
-
+  const altFamily_tv = 'familia sentada mirando un tv'
   return (
     <>
       <div className="container">
+        <div className='tv'>
+          {/* <section className="parallax-bg img_perro1" >
+          </section>
+          <section className="parallax-content">
+            <div>
+              <h1>Parallax section</h1>
+              <p>Este texto acompaña al background parallax en su propia sección</p>
+            </div>
+          </section> */}
 
-        {/* family */}
-        <section className="parallax-bgF family_tv"></section>
-        <section className="parallax-content">
-          <div>
-            <h1>Parallax section</h1>
-            <p>Este texto acompaña al background parallax en su propia sección</p>
-          </div>
-        </section>
 
-        {/* // -- Seccion NO parallax */}
+        </div>
+        <img className='family_tv' src={family_tv} alt={altFamily_tv} />
+
         <section className="no-parallax-section bg-1">
           <div className="no-parallax-content">
             <h1>No-parallax section</h1>
             <p>En esta sección incluimos la clase "bg-1" para otorgarle un color de fondo.</p>
           </div>
         </section>
-        {/* // -- Seccion parallax -- */}
-        <section className="parallax-bg img_perro1" > </section>
-        <section className="parallax-content">
-          <div>
-            <h1>Parallax section</h1>
-            <p>Este texto acompaña al background parallax en su propia sección</p>
-          </div>
-        </section>
-
       </div >
       {/* // -- End Container */}
     </>
